35300590ee7b2fc850bdfda86199ccdee863798a
Juan Pablo <juanpablosolana@gmail.com>
2021-02-16 07:35:36 -0600

    ignorando archivos

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBZ0lTxtN8wUJIhSQvBRL8xT-hWPW0lTyA",
+  authDomain: "huerto-fee51.firebaseapp.com",
+  databaseURL: "https://huerto-fee51.firebaseio.com",
+  projectId: "huerto-fee51",
+  storageBucket: "huerto-fee51.appspot.com",
+  messagingSenderId: "564399783086",
+  appId: "1:564399783086:web:ae71b8713f73e7b52a413f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
